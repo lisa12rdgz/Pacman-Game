c4dd6d0e76cad0e6771758c0a3c77e75b21fae8c
AdmiredButton67 <94034532+AdmiredButton67@users.noreply.github.com>
2022-05-10 19:44:39 -0500

    Fantasmas mas inteligentes

--- Pacman.py
+++ Pacman.py
@@ -140,14 +140,6 @@ def move():
 
             plan = choice(options)
                 
-
-            #options = [
-            ##    vector(5, 0),
-            #    vector(-5, 0),
-            #    vector(0, 5),
-            #    vector(0, -5),
-            #]
-            #plan = choice(options)
             course.x = plan.x
             course.y = plan.y
 
